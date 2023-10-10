83f02d9e8bc17d6ddbdba541e6396d0b8939ba03
Landon <landonp95@gmail.com>
2023-10-10 00:25:36 -0400

    format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,15 +10,15 @@ module.exports = {
     themes: [
       {
         eastwood: {
-          "primary": "#ce000c",
-          "secondary": "#003f7f",
-          "accent": "#666666",
-          "neutral": "#eaeaea",
+          primary: "#ce000c",
+          secondary: "#003f7f",
+          accent: "#666666",
+          neutral: "#eaeaea",
           "base-100": "#eaeaea",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
         },
       },
     ],
