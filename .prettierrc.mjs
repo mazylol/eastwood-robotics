1981bcb29aa83d477d4cf72ba373d13019c1a1e8
Landon <landonp95@gmail.com>
2023-10-12 15:19:17 -0400

    formatting works now

---
+++ .prettierrc.mjs
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+export default {
+    endOfLine: "lf",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
 
-  daisyui: {
-    themes: [
-      {
-        eastwood: {
-          primary: "#ce000c",
-          secondary: "#003f7f",
-          accent: "#666666",
-          neutral: "#eaeaea",
-          "base-100": "#eaeaea",
-          info: "#3abff8",
-          success: "#36d399",
-          warning: "#fbbd23",
-          error: "#f87272",
-        },
-      },
-    ],
-  },
+    daisyui: {
+        themes: [
+            {
+                eastwood: {
+                    primary: "#ce000c",
+                    secondary: "#003f7f",
+                    accent: "#666666",
+                    neutral: "#eaeaea",
+                    "base-100": "#eaeaea",
+                    info: "#3abff8",
+                    success: "#36d399",
+                    warning: "#fbbd23",
+                    error: "#f87272",
+                },
+            },
+        ],
+    },
 };
