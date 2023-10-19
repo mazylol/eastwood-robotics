c7bf8b0b169bbd849925d1dc175fe1633d81e10c
Landon <landonp95@gmail.com>
2023-10-19 17:02:25 -0400

    gave up on filtering by tag for now, terribly complicated

--- src/components/team/Main.tsx
+++ src/components/team/Main.tsx
@@ -1,18 +1,12 @@
 import { type Member } from "../../util/json.ts";
 import Card from "./Card.tsx";
 import { useState } from "react";
-import Tag from "./Tag.tsx";
 
 export default function Main(props: { members: Member[] }) {
     let [members, setMembers] = useState(props.members);
 
     return (
         <main>
-            <div className="mr-4">
-                <ul className="grid grid-flow-col gap-4 justify-end">
-                    <Tag>Hei</Tag>
-                </ul>
-            </div>
             <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 mx-4 my-4 mt-16">
                 {members.map((member) => (
                     <Card
