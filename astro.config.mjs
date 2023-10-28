b1ad9447075fdec2f2e50bb822508ae9490cac05
Mazy <landonp95@gmail.com>
2023-10-28 20:32:55 +0000

    Removed react. Hardcoding for image opimization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
+  integrations: [tailwind(), compressor({gzip: true, brotli: false})]
 });
